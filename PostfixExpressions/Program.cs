061003b55770fe5b83b2a16f504283070c7d4aee
Muhammad Raffay <muhammadraffay@trentu.ca>
2024-09-17 18:20:04 -0400

    Add template code

--- PostfixExpressions/Program.cs
+++ PostfixExpressions/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Collections.Generic;
+
+namespace Expressions___Strings
+{
+    public class PostFixExpression
+    {
+        //...
+        private List<object> L;
+
+        //create but donot initialize a List objects
+        //call methods Analyzer and Convert
+        public PostFixExpression(string s)
+        {
+
+        }
+
+        // Process the called string character by character
+        // insert operands & integers to a list L.
+        // otherwise throw an arguement
+        private void Analyzer(string s)
+        {
+
+        }
+
+        //Covert the List created to a postfix expression
+        private void Convert()
+        {
+
+        }
+
+        //return the result of the current postfix expression using an operand(floating point)stack
+        public float Evaluate()
+        {
+            private double total;
+
+        // Step 1: Loop through both lists at the same time
+
+        // Step 2: Execute the first operand with the respective first two constants in the list
+
+        // Step 3: Update the the total as you go
+
+    }
+
+    //returns the string representation of the postfix expression
+    public override string ToString()
+    {
+
+    }
+
+
+
+
+
+    //Main
+    public static void Main(string args)
+    {
+        // 1.reads in a string s from the console
+
+        // 2.defines and evaluates an instance of PostFixExpression
+    }
+}
+}
+
+
+
+
